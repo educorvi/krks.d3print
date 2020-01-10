a1c23625b4fab1419bc4a00a403ef0e3c94dc3b4
pascalpaul <pascalpaul@educorvi.de>
2020-01-10 14:59:57 +0100

    added temperature request

--- src/krks/d3print/views/druckerview.py
+++ src/krks/d3print/views/druckerview.py
@@ -1,10 +1,30 @@
 # -*- coding: utf-8 -*-
-
+import requests
+from time import sleep
 from krks.d3print import _
 from Products.Five.browser import BrowserView
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+post_headers={
+    'Host': '192.168.86.56:5001', #Aus Content-Objekt
+    'X-Api-Key': '02AC33DA679D4A93BA6BF9EAB1CF01CC', #Aus Content-Objekt
+    'Content-Type': 'application/json' #konstant
+    }
+
+get_headers={
+    'Host': '192.168.86.56:5001', #Aus Content-Objekt
+    'X-Api-Key': '02AC33DA679D4A93BA6BF9EAB1CF01CC' #Aus Content-Objekt
+    }
+
+connect_json={
+    'command': 'connect',
+    'port': '/dev/ttyUSB0', #Aus Content-Objekt (Drop-Down)
+    'baudrate': 250000, #Aus Content-Objekt (Drop-Down)
+    'printerProfile': '_default',
+    'save': 'true',
+    'autoconnect': 'true'
+    }
 
 class Druckerview(BrowserView):
     # If you want to define a template here, please remove the template from
@@ -15,3 +35,25 @@ class Druckerview(BrowserView):
         # Implement your own actions:
         self.msg = _(u'A small message')
         return self.index()
+
+    def connect_printer(self):
+
+        tooltemp = 'k.A'
+        bedtemp = 'k.A'
+
+        result = requests.post('http://192.168.86.56:5001/api/connection', headers=post_headers, json=connect_json)
+        print(result.status_code)
+        if result.status_code == 204:
+            sleep(5)
+            data = requests.get('http://192.168.86.56:5001/api/printer', headers=get_headers)
+            datadict = data.json()
+
+            if datadict:
+                temp = datadict.get("temperature")
+                if temp:
+                    tooltemp = temp['tool0']['actual']
+                    bedtemp = temp['bed']['actual']
+
+        else:
+            print("Error")
+        return {'tooltemp':tooltemp, 'bedtemp':bedtemp}
