5586d19fb5022a6e1c8d1c1c4b075feec3b861fb
seppowalther <seppo.walther@educorvi.de>
2020-02-13 13:42:22 +0100

    Cleared up upload_view

--- src/krks/d3print/views/upload_view.py
+++ src/krks/d3print/views/upload_view.py
@@ -10,26 +10,17 @@ class UploadView(BrowserView):
     def __call__(self):
 
         blob = self.context.printmodell
-        #ploneio = stream_data(blob)
-        #myio = open(ploneio.name, 'rb')
-        #myio = io.BytesIO(blob.data)
         temppath = '/tmp/%s' %blob.filename
         tempfile = open(temppath, 'wb')
         tempfile.write(blob.data)
         tempfile.close()
         myio = open(temppath, 'rb')
-        #filename = 'max.gcode'
-        #import pdb;pdb.set_trace()
         filename = blob.filename
         fle = {'file':myio, 'filename':filename}
 
         printer = api.content.get(UID=self.context.drucker)
 
-        # printer.ipaddresse (+ Port)
-
-        #Informationen muessen aus Printer-Objekt bezogen werden
         url='http://'+printer.ipaddresse+':'+printer.port+'/api/files/{}'.format('local')
-        #URL?
         payload={'select': 'true','print': 'true' }
         header={'X-Api-Key': printer.apikey}
         response = requests.post(url, files=fle,data=payload,headers=header)
